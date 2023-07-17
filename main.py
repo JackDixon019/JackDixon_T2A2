5053a25b0570c514d49edf1b704ba8bc0f333bed
JackDixon019 <jack.dixon019@gmail.com>
2023-07-17 18:19:53 +1000

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask
+
+from init import bcrypt, db, jwt, ma
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Stop json keys from sorting
+    app.json.sort_keys = False
+
+    # Get variables from .env file
+    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL")
+    app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+
+    # Calls objects within function to prevent double-import errors
+    db.init_app(app)
+    ma.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+
+    return app
+
