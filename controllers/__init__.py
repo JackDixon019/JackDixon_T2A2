a8e2f50bb044b18dc110e6b56c56ec8f20a8e905
JackDixon019 <jack.dixon019@gmail.com>
2023-07-20 10:16:07 +1000

    Add location to db seeding

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,7 +1,9 @@
 from controllers.cli_controller import db_commands
 from controllers.bird_controller import birds_bp
+from controllers.location_controller import locations_bp
 
 registerable_controllers = [
     db_commands,
-    birds_bp
+    birds_bp,
+    locations_bp,
 ]
